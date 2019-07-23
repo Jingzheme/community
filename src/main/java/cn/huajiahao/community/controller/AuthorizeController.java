3e02ef0779ccc823eb826a18768809d0f4137e01
Jingzheme <1174463270@qq.com>
2019-07-23 11:12:13 +0800

    登录界运用了thyeleaf面的动态跳转

--- src/main/java/cn/huajiahao/community/controller/AuthorizeController.java
+++ src/main/java/cn/huajiahao/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @Description TODO
  * @Date 2019/7/22 16:43
@@ -30,7 +32,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public  String callback(@RequestParam(name="code") String code,
-                            @RequestParam(name="state") String state){
+                            @RequestParam(name="state") String state,
+                            HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -39,7 +42,15 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user!=null){
+            // 登录成功，写cookie，session
+            request.getSession().setAttribute("user",user);
+            //重定向
+            return "redirect:/";
+
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
